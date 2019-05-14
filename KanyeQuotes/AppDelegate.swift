ac29940c7b73883bb7e1566f11711d6b5d8f080c
Mike Horn <mikelaurencehorn@gmail.com>
2019-05-14 17:06:13 -0400

    update with all branch info

--- KanyeQuotes/AppDelegate.swift
+++ KanyeQuotes/AppDelegate.swift
@@ -2,45 +2,68 @@
 //  AppDelegate.swift
 //  KanyeQuotes
 //
-//  Created by Michael Horn on 5/14/19.
+//  Created by Michael Horn on 5/10/19.
 //  Copyright © 2019 Mike Horn. All rights reserved.
 //
 
 import UIKit
+import Branch
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // if you are using the TEST key
+        //        Branch.setUseTestBranchKey(true)
+        // listener for Branch Deep Link data
+        Branch.getInstance().initSession(launchOptions: launchOptions) { (params, error) in
+            // do stuff with deep link data (nav to page, display content, etc)
+            print(params as? [String: AnyObject] ?? {})
+        }
         return true
     }
-
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        Branch.getInstance().application(app, open: url, options: options)
+        return true
+    }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        // handler for Universal Links
+        Branch.getInstance().continue(userActivity)
+        return true
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        // handler for Push Notifications
+        Branch.getInstance().handlePushNotification(userInfo)
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    
 }
 
--- KanyeQuotes/ViewController.swift
+++ KanyeQuotes/ViewController.swift
@@ -2,19 +2,52 @@
 //  ViewController.swift
 //  KanyeQuotes
 //
-//  Created by Michael Horn on 5/14/19.
+//  Created by Michael Horn on 5/10/19.
 //  Copyright © 2019 Mike Horn. All rights reserved.
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var kanyeQuoteLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loadUpKanyeQuote()
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    var headers: HTTPHeaders {
+        get {
+            return [
+                "Accept": "application/json"
+            ]
+        }
+    }
+    
+    func loadUpKanyeQuote() {
+        let url = "https://api.kanye.rest"
+        var kanyeQuote = ""
+        Alamofire.request(url, method: .get, encoding: JSONEncoding.default, headers: headers)
+            .responseJSON { response in
+                if let data: Data = response.data {
+                    guard let json = try? JSON(data: data) else { return }
+                    kanyeQuote = json["quote"].stringValue
+                    self.kanyeQuoteLabel.text = kanyeQuote
+                }
+        }
+    }
+    
+    @IBAction func changeKanyeQuote(sender: UIButton) {
+        loadUpKanyeQuote()
+    }
+    
 }
 
