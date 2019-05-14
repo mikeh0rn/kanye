558a8e62c61c5460f1b69e8a0504dae52d8a15a2
Mike Horn <mikelaurencehorn@gmail.com>
2019-05-14 16:52:48 -0400

    create project

---
+++ KanyeQuotes/KanyeQuotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KanyeQuotes
+//
+//  Created by Michael Horn on 5/14/19.
+//  Copyright © 2019 Mike Horn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
